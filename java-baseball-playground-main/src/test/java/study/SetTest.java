459e947400f9ea9b0d884bfc1208946e95ae8621
seongbinko <wnrhd1082@gmail.com>
2022-07-24 16:39:55 +0900

    0. 학습테스트 실습

---
+++ java-baseball-playground-main/src/test/java/study/SetTest.java
@@ -0,0 +1,59 @@
+package study;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SetTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void size() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @Test
+    void contains() { // 중복
+        assertThat(numbers.contains(1)).isTrue();
+        assertThat(numbers.contains(2)).isTrue();
+        assertThat(numbers.contains(3)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void contains2(int nums) {
+        assertThat(numbers).contains(nums);
+
+        assertTrue(numbers.contains(nums));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void usingContains_true_false(int input, boolean expected) {
+
+        assertEquals(numbers.contains(input), expected);
+
+        assertThat(numbers.contains(input)).isEqualTo(expected);
+    }
+}
--- java-baseball-playground-main/src/test/java/study/StringTest.java
+++ java-baseball-playground-main/src/test/java/study/StringTest.java
@@ -1,13 +1,51 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    void split() {
+        String[] actual = "1,2".split(",");
+        assertThat(actual).contains("1", "2");
+    }
+
+    @Test
+    void split2() {
+        String[] actual = "1".split(",");
+        assertThat(actual).containsExactly("1");
+    }
+
+    @Test
+    void substring() {
+        // when
+        String s = "(1,2)";
+        String actual = s.substring(s.indexOf("(") + 1, s.indexOf(")"));
+        assertThat(actual).isEqualTo("1,2");
+    }
+
+    /**
+     * 자주 발생하는 Exception의 경우 Exception별 메서드 제공하고 있음
+     * assertThatIllegalArgumentException()
+     * assertThatIllegalStateException()
+     * assertThatIOException()
+     * assertThatNullPointerException()
+     */
+    @Test
+    @DisplayName("특정 위치의 문자 가져오기")
+    void charAt() {
+        String s = "abc";
+        assertThatThrownBy(() -> s.charAt(s.length()))
+                .isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: " + s.length());
     }
 }
